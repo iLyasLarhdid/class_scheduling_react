0870b494ab3f8568992e003370d6e49ee70336d7
ilyas <larhdidok@gmail.com>
2022-05-23 00:05:37 +0100

    properties for heroku

--- src/properties.js
+++ src/properties.js
@@ -1,7 +1,7 @@
 const properties = {
-    //host:"https://chamy-livraison.herokuapp.com",
+    host:"https://isgascheduler.herokuapp.com",
     //host:"http://192.168.1.35:8080",
-    host:"http://localhost:8080"    
+    //host:"http://localhost:8080"    
     //host:"http://192.168.142.49:8080",
 }
 export default properties
