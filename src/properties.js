b4bdf623a3d4cc03899a6f9c81760ca1fed99efa
ilyas <larhdidok@gmail.com>
2023-02-12 02:08:33 +0000

    change the back end

--- src/properties.js
+++ src/properties.js
@@ -1,8 +1,8 @@
 const properties = {
     //host:"https://isgascheduler.herokuapp.com",
     //host:"http://192.168.1.35:8080",
-    host:"http://localhost:8080",
-    //host:"https://exam-scheduler.up.railway.app"  
+    //host:"http://localhost:8080",
+    host:"https://exam-scheduler.up.railway.app"  
     //host:"http://192.168.142.49:8080",
 }
 export default properties
